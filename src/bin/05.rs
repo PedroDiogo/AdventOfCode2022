7fd72ecb2448445322812d80b88759d733b64f1e
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2022-12-05 20:15:22 +0000

    Day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -55,8 +55,50 @@ pub fn part_one(input: &str) -> Option<String> {
     Some(stacks.iter().map(|stack| stack.last().unwrap()).join(""))
 }
 
-pub fn part_two(input: &str) -> Option<usize> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let (starting_stacks, moves) = input.split("\n\n").collect_tuple().unwrap();
+
+    let num_of_columns = starting_stacks
+        .lines()
+        .last()
+        .unwrap()
+        .split_whitespace()
+        .last()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+
+    let mut stacks = vec![Stack::new(); num_of_columns];
+    for line in starting_stacks.lines().dropping_back(1) {
+        let line = line.as_bytes();
+        for idx in 0..num_of_columns {
+            let character_at_idx = line[idx * 4 + 1] as char;
+            if character_at_idx != ' ' {
+                stacks[idx].insert(0, character_at_idx);
+            }
+        }
+    }
+
+    let moves = moves
+        .lines()
+        .map(|line| {
+            let split = line.split_whitespace().collect_vec();
+            Move {
+                quantity: split[1].parse().unwrap(),
+                from: split[3].parse::<usize>().unwrap() - 1,
+                to: split[5].parse::<usize>().unwrap() - 1,
+            }
+        })
+        .collect_vec();
+
+    for m in moves {
+        let (bottom, top) = stacks[m.from].split_at(stacks[m.from].len() - m.quantity);
+        let bottom = bottom.to_vec();
+        let mut top = top.to_vec();
+        stacks[m.from] = bottom;
+        stacks[m.to].append(&mut top);
+    }
+    Some(stacks.iter().map(|stack| stack.last().unwrap()).join(""))
 }
 
 fn main() {
@@ -78,6 +120,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some("MCD".to_string()));
     }
 }
