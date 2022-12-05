057a89b41d18e88c96728165a7222d735f8ba004
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2022-12-05 20:40:18 +0000

    Day 5 cleanup

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -9,40 +9,7 @@ struct Move {
     to: usize,
 }
 pub fn part_one(input: &str) -> Option<String> {
-    let (starting_stacks, moves) = input.split("\n\n").collect_tuple().unwrap();
-
-    let num_of_columns = starting_stacks
-        .lines()
-        .last()
-        .unwrap()
-        .split_whitespace()
-        .last()
-        .unwrap()
-        .parse::<usize>()
-        .unwrap();
-
-    let mut stacks = vec![Stack::new(); num_of_columns];
-    for line in starting_stacks.lines().dropping_back(1) {
-        let line = line.as_bytes();
-        for idx in 0..num_of_columns {
-            let character_at_idx = line[idx * 4 + 1] as char;
-            if character_at_idx != ' ' {
-                stacks[idx].insert(0, character_at_idx);
-            }
-        }
-    }
-
-    let moves = moves
-        .lines()
-        .map(|line| {
-            let split = line.split_whitespace().collect_vec();
-            Move {
-                quantity: split[1].parse().unwrap(),
-                from: split[3].parse::<usize>().unwrap() - 1,
-                to: split[5].parse::<usize>().unwrap() - 1,
-            }
-        })
-        .collect_vec();
+    let (mut stacks, moves) = parse_input(input);
 
     for m in moves {
         let (bottom, top) = stacks[m.from].split_at(stacks[m.from].len() - m.quantity);
@@ -56,28 +23,40 @@ pub fn part_one(input: &str) -> Option<String> {
 }
 
 pub fn part_two(input: &str) -> Option<String> {
-    let (starting_stacks, moves) = input.split("\n\n").collect_tuple().unwrap();
+    let (mut stacks, moves) = parse_input(input);
 
+    for m in moves {
+        let (bottom, top) = stacks[m.from].split_at(stacks[m.from].len() - m.quantity);
+        let bottom = bottom.to_vec();
+        let mut top = top.to_vec();
+        stacks[m.from] = bottom;
+        stacks[m.to].append(&mut top);
+    }
+    Some(stacks.iter().map(|stack| stack.last().unwrap()).join(""))
+}
+
+fn parse_input(input: &str) -> (Vec<Stack>, Vec<Move>) {
+    let (starting_stacks, moves) = input.split("\n\n").collect_tuple().unwrap();
     let num_of_columns = starting_stacks
         .lines()
         .last()
         .unwrap()
         .split_whitespace()
-        .last()
-        .unwrap()
-        .parse::<usize>()
-        .unwrap();
+        .count();
 
-    let mut stacks = vec![Stack::new(); num_of_columns];
-    for line in starting_stacks.lines().dropping_back(1) {
-        let line = line.as_bytes();
-        for idx in 0..num_of_columns {
-            let character_at_idx = line[idx * 4 + 1] as char;
-            if character_at_idx != ' ' {
-                stacks[idx].insert(0, character_at_idx);
+    let stacks = starting_stacks.lines().dropping_back(1).fold(
+        vec![Stack::new(); num_of_columns],
+        |mut current_stacks, line| {
+            let line = line.as_bytes();
+            for idx in 0..num_of_columns {
+                let character_at_idx = line[idx * 4 + 1] as char;
+                if character_at_idx != ' ' {
+                    current_stacks[idx].insert(0, character_at_idx);
+                }
             }
-        }
-    }
+            current_stacks
+        },
+    );
 
     let moves = moves
         .lines()
@@ -90,15 +69,7 @@ pub fn part_two(input: &str) -> Option<String> {
             }
         })
         .collect_vec();
-
-    for m in moves {
-        let (bottom, top) = stacks[m.from].split_at(stacks[m.from].len() - m.quantity);
-        let bottom = bottom.to_vec();
-        let mut top = top.to_vec();
-        stacks[m.from] = bottom;
-        stacks[m.to].append(&mut top);
-    }
-    Some(stacks.iter().map(|stack| stack.last().unwrap()).join(""))
+    (stacks, moves)
 }
 
 fn main() {
