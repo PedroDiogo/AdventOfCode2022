c51b4cb71d161fcf4252f9e64b5c3b2f8cd736b9
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2022-12-07 22:15:05 +0000

    Day 7 part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(Debug)]
 struct Node<T>
 where
@@ -64,7 +66,64 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let input = input.split("$").map(|l| l.trim()).filter(|i| !i.is_empty());
+    let mut filesystem = vec![Node {
+        idx: 0,
+        val: 0,
+        parent: None,
+        children: vec![],
+    }];
+    let mut current_directory = 0;
+
+    for command_output in input {
+        let mut command_output = command_output.lines();
+        let command = command_output.next().unwrap();
+
+        if command == "ls" {
+            for ls_line in command_output {
+                if let Some(size) = ls_line
+                    .split_whitespace()
+                    .next()
+                    .unwrap()
+                    .parse::<usize>()
+                    .ok()
+                {
+                    filesystem[current_directory].val += size;
+                }
+            }
+        } else {
+            match command.split_whitespace().last() {
+                Some("..") => current_directory = filesystem[current_directory].parent.unwrap(),
+                Some("/") => current_directory = 0,
+                _ => {
+                    let new_directory = Node {
+                        idx: filesystem.len(),
+                        val: 0,
+                        parent: Some(current_directory),
+                        children: vec![],
+                    };
+                    let new_idx = new_directory.idx;
+                    filesystem[current_directory].children.push(new_idx);
+                    filesystem.push(new_directory);
+                    current_directory = new_idx;
+                }
+            }
+        }
+    }
+
+    let total_sizes = filesystem
+        .iter()
+        .map(|directory| calculate_total_size(&filesystem, directory.idx))
+        .collect_vec();
+
+    let root_file_size = total_sizes.iter().max().unwrap();
+    let unused = 70000000 - root_file_size;
+    let need = 30000000 - unused;
+
+    total_sizes
+        .into_iter()
+        .filter(|total_size| *total_size >= need)
+        .min()
 }
 
 fn calculate_total_size(filesystem: &[Node<usize>], node: usize) -> usize {
