95128b678d657f6155b58cdf797de1eb29615f1b
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2022-12-23 15:17:37 +0000

    Day 18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use advent_of_code::helpers::GetNumbers;
 use itertools::Itertools;
@@ -22,6 +22,27 @@ impl AddXYZ for Position {
 
     fn add_z(&self, z: &isize) -> Position {
         (self.0, self.1, self.2 + z)
+    }
+}
+
+trait Neighbours {
+    fn neighbours(&self) -> Vec<Position>;
+}
+
+impl Neighbours for Position {
+    fn neighbours(&self) -> Vec<Self> {
+        let deltas = vec![
+            (1, 0, 0),
+            (0, 1, 0),
+            (-1, 0, 0),
+            (0, -1, 0),
+            (0, 0, 1),
+            (0, 0, -1),
+        ];
+        deltas
+            .into_iter()
+            .map(|delta| (delta.0 + self.0, delta.1 + self.1, delta.2 + self.2))
+            .collect_vec()
     }
 }
 
@@ -99,11 +120,103 @@ pub fn part_one(input: &str) -> Option<usize> {
             .or_insert(1);
     }
 
+    // println!("{:?}", sides);
     Some(sides.into_iter().filter(|(_, v)| v == &1).count())
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let coordinates: Vec<Position> = input
+        .lines()
+        .filter_map(|line| {
+            line.number_by_separators(&[','])
+                .into_iter()
+                .collect_tuple()
+        })
+        .collect_vec();
+    let (min_x, max_x) = match coordinates.iter().minmax_by_key(|c| c.0) {
+        itertools::MinMaxResult::MinMax(min, max) => (min.0, max.0),
+        _ => unimplemented!(),
+    };
+    let (min_y, max_y) = match coordinates.iter().minmax_by_key(|c| c.1) {
+        itertools::MinMaxResult::MinMax(min, max) => (min.1, max.1),
+        _ => unimplemented!(),
+    };
+    let (min_z, max_z) = match coordinates.iter().minmax_by_key(|c| c.2) {
+        itertools::MinMaxResult::MinMax(min, max) => (min.2, max.2),
+        _ => unimplemented!(),
+    };
+
+    let (min_x, max_x) = (min_x - 1, max_x + 1);
+    let (min_y, max_y) = (min_y - 1, max_y + 1);
+    let (min_z, max_z) = (min_z - 1, max_z + 1);
+
+    let mut map = HashMap::new();
+    map.insert((min_x, min_y, min_z), '-');
+    for coordinate in coordinates.clone() {
+        map.insert(coordinate, '#');
+    }
+    for z in min_z..=max_z {
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                let c = map.get(&(x, y, z));
+                if c.is_some() {
+                    continue;
+                }
+                let any_outside = (x, y, z)
+                    .neighbours()
+                    .iter()
+                    .filter_map(|m| map.get(m))
+                    .any(|m| m == &'-');
+                if any_outside {
+                    map.insert((x, y, z), '-');
+                }
+            }
+        }
+    }
+
+    for z in (min_z..=max_z).rev() {
+        for y in (min_y..=max_y).rev() {
+            for x in (min_x..=max_x).rev() {
+                let c = map.get(&(x, y, z));
+                if c.is_some() {
+                    continue;
+                }
+                let any_outside = (x, y, z)
+                    .neighbours()
+                    .iter()
+                    .filter_map(|m| map.get(m))
+                    .any(|m| m == &'-');
+                if any_outside {
+                    map.insert((x, y, z), '-');
+                }
+            }
+        }
+    }
+
+    for z in min_z..=max_z {
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                print!("{}", map.get(&(x, y, z)).unwrap_or(&'.'))
+            }
+            println!("");
+        }
+        println!("///");
+    }
+    println!("x: {}->{}", min_x, max_x);
+    println!("y: {}->{}", min_y, max_y);
+    println!("z: {}->{}", min_z, max_z);
+
+    let mut a = 0;
+    for coordinate in &coordinates {
+        a += coordinate
+            .neighbours()
+            .iter()
+            .filter_map(|n| map.get(n))
+            .filter(|c| *c == &'-')
+            .count();
+    }
+
+    Some(a)
 }
 
 fn main() {
