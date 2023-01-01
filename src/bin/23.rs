f109288f4116f75325b4a2c91ef1433964cac7c2
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2023-01-01 19:59:24 +0000

    Day 23 part 2

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -138,12 +138,81 @@ pub fn part_one(input: &str) -> Option<isize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
+    let mut elves: HashSet<Position> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.char_indices()
+                .filter(|(_x, c)| c == &'#')
+                .map(|(x, _c)| (x as isize, y as isize))
+                .collect::<Vec<Position>>()
+        })
+        .collect();
+    // println!("Elves: {:?}", elves);
+
+    type NeightboursFn = for<'r> fn(&'r Position) -> Vec<Position>;
+    let mut moves: [(NeightboursFn, Position); 4] = [
+        (Neighbours::north_neighbours, (0, -1)),
+        (Neighbours::south_neighbours, (0, 1)),
+        (Neighbours::west_neighbours, (-1, 0)),
+        (Neighbours::east_neighbours, (1, 0)),
+    ];
+
+    for round in 0..100000 {
+        let starting_map = elves.clone();
+        let mut new_moves: HashMap<Position, Vec<Position>> = HashMap::new();
+        let mut number_of_moves = 0;
+        for elf in &starting_map {
+            let has_neighbours = elf
+                .all_neighbours()
+                .iter()
+                .any(|n| starting_map.contains(n));
+
+            if has_neighbours {
+                let a = moves
+                    .iter()
+                    .find(|m| m.0(&elf).iter().all(|n| !starting_map.contains(n)));
+                if let Some((_, delta)) = a {
+                    let dst = (elf.0 + delta.0, elf.1 + delta.1);
+
+                    new_moves.entry(dst).or_insert(Vec::new()).push(*elf);
+                    number_of_moves += 1;
+                    // println!("Elf {:?} moves to {:?}", elf, dst);
+                } else {
+                    // println!("Elf {:?} does not move", elf);
+                    new_moves.entry(*elf).or_insert(Vec::new()).push(*elf);
+                }
+            } else {
+                // println!("Elf {:?} does not move. No neighbours.", elf);
+                new_moves.entry(*elf).or_insert(Vec::new()).push(*elf);
+            }
+        }
+        if number_of_moves == 0 {
+            return Some(round + 1);
+        }
+        // println!("{:?}", new_moves);
+        let mut new_map: HashSet<Position> = HashSet::new();
+        for (dst, possible_elves) in new_moves.iter() {
+            if possible_elves.len() == 1 {
+                new_map.insert(*dst);
+            } else {
+                // println!("More than one");
+                for elf in possible_elves {
+                    new_map.insert(*elf);
+                }
+            }
+        }
+        elves = new_map;
+        moves = [moves[1], moves[2], moves[3], moves[0]];
+        // println!("{:?}", elves);
+    }
+
     None
 }
 
 fn main() {
     let input = &advent_of_code::read_file("inputs", 23);
-    advent_of_code::solve!(1, part_one, input);
+    // advent_of_code::solve!(1, part_one, input);
     advent_of_code::solve!(2, part_two, input);
 }
 
